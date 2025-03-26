39bc5b8f3562871f68e01f50d58975b1986f83bb
ChoshenHindi <Choshenhindi@gmail.com>
2025-03-26 14:25:29 +0200

    Update Inquiry Router with RUD Requests
    Updated inquiryRoutes with the following requests:
     - Fetch inquiries by user
     - Fetch all inquiries (for admin)
     - Update inquiry status
     - Delete inquiry

--- server/routes/inquiryRoutes.js
+++ server/routes/inquiryRoutes.js
@@ -33,4 +33,80 @@ router.post('/send-inquiry', async (req, res) => {
     }
 });
 
+// Fetch all inquiries
+router.get('/all-inquiries', async (req, res) => {
+    try {
+        const inquiries = await Inquiry.find()
+            .populate('user', '-password -role')
+            .populate('apartment')
+            .populate({
+                path: 'apartment.owner',
+                select: '-password -role'
+            });
+
+        if (!inquiries || inquiries.length === 0) {
+            return res.status(404).send({ message: 'No inquiries found.' });
+        }
+
+        res.status(200).send({ message: 'All inquiries retrieved successfully.', inquiries: inquiries });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: 'Server error. Please try again later.' });
+    }
+});
+
+// Fetch all inquiries of a user
+router.get('/user-inquiries/:id', async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const inquiries = await Inquiry.find({ user: userId }).populate('apartment', '-owner -address.apartmentNumber');
+
+        if (!inquiries || inquiries.length === 0) {
+            return res.status(404).send({ message: 'No inquiries sent by this user.' });
+        }
+
+        res.status(200).send({ message: 'Inquiries retrieved successfully.', inquiries: inquiries });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: 'Server error. Please try again later.' });
+    }
+});
+
+// Update inquiry status
+router.post('/change-inquiry-status', async (req, res) => {
+    const { inquiryId, status } = req.body;
+
+    try {
+        const updatedInquiry = await Inquiry.findByIdAndUpdate(inquiryId, { status: status }, { new: true });
+
+        if (!updatedInquiry) {
+            return res.status(404).send({ message: 'Inquiry not found.' });
+        }
+
+        res.status(200).send({ message: 'Inquiry status updated successfully.' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: 'Server error. Please try again later.' });
+    }
+});
+
+// Delete an inquiry
+router.delete('/delete-inquiry/:id', async (req, res) => {
+    const inquiryId = req.params.id;
+
+    try {
+        const inquiry = await Inquiry.findByIdAndDelete(inquiryId);
+
+        if (!inquiry) {
+            return res.status(404).send({ message: 'Inquiry not found' });
+        }
+
+        res.status(200).send({ message: 'Inquiry successfully deleted' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: 'Server error. Please try again later.' });
+    }
+});
+
 module.exports = router;
